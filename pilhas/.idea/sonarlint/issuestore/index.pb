d864045a28f1afa209a236ad5f28c030e10abbac
Bruno <brunobilheri@hotmail.com>
2021-12-23 08:56:14 -0300

    Exercise 01 finished

--- pilhas/.idea/sonarlint/issuestore/index.pb
+++ pilhas/.idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,13 @@
 
 W
-'src/com/estruturadados/pilha/Pilha.java,3\c\3c8cf085b704f053796cb9508ba2d224c6711f7d
-W
 'src/com/estruturadados/pilha/Lista.java,4\7\471a5ea7dbb308227362c0ca885cf98221ffdecd
 X
 (src/com/estruturadados/pilha/Lista2.java,9\7\977d8a203c66f5005c01b4f4aed5667c4eba6155
+I
+src/teste/PilhaTeste.java,1\6\16814d3a729e726cc447ad65adac45f4b556ce3a
+O
+src/base/EstruturaEstatica.java,3\5\351c36a896f751515e070ff0b4ddf4f6969e7826
+W
+'src/com/estruturadados/pilha/Pilha.java,3\c\3c8cf085b704f053796cb9508ba2d224c6711f7d
+D
+src/vetor/Pilha.java,8\d\8d5df95cebeff816da36f2b2d9334267256a1bf2
--- pilhas/src/labs/exer01.java
+++ pilhas/src/labs/exer01.java
@@ -6,13 +6,37 @@ import java.util.Scanner;
 
 public class exer01 {
     public static void main(String[] args) {
+
         Pilha<Integer> pilha = new Pilha<Integer>();
 
         Scanner input = new Scanner(System.in);
 
         for(int i = 0; i <= 10; i++){
             System.out.println("Digite um numero :");
-            int num = input.nextInt();
+            int number = input.nextInt();
+
+            if(number % 2 == 0){
+                System.out.println("Empilhando o número :" + number);
+                pilha.empilha(number);
+            } else {
+                Integer desempilha = pilha.desempilhar();
+                if(desempilha == null){
+                    System.out.println("Pilha está vazia!");
+                } else {
+                    System.out.println("Número impar, desempilhando um elemento da pilha: " + pilha.desempilhar());
+                }
+            }
         }
+
+        System.out.println("Todos os números foram lidos, desempilhando números da pilha");
+
+        while(!pilha.estaVazio()){
+            Integer desempilha = pilha.desempilhar();
+
+            System.out.println("Desenpilhando um elemento da pilha " + desempilha);
+        }
+        System.out.println("Todos os elementos foram desempilhados");
+
+
     }
 }
